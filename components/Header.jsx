9a049df260fdb5629cfe0bcb64af2c14bc1149c2
jkClyde <aquin.randall@gmail.com>
2025-01-22 03:48:18 +0800

    header padding fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,18 +5,17 @@ import DynamicCard from "./Card"
 
 const Header = ({children}) => {
   return (
-    <div className=" bg-hero-pattern bg-cover bg-no-repeat">
+    <div className=" bg-hero-pattern bg-cover bg-no-repeat min-h-screen ">
         <Navbar/>
-        <div id="Banner" className="font-Onest text-primary flex flex-col items-center ">
-          <h1 className=" text-[64px] max-w-[1320px]  text-center mx-auto pt-[100px] md:pt-[225px] leading-[1.2]">
+        <div id="Banner"  className="font-Onest text-primary flex flex-col items-center justify-center min-h-screen text-center">
+          <h1 className=" text-[64px] max-w-[1320px]  text-center mx-auto  leading-[1.2]">
              Hey, I’m Clyde. I'm a<span className="inline md:block"> Web Developer</span> 
           </h1>
           <p className="mx-auto text-center max-w-[680px] pt-[20px] pb-[20px]">
           I focus on creating modern, responsive websites and web applications that deliver seamless user experiences and meet your unique needs.
           </p>
-          <Button text="Lets Work Together" width="195px" />
-          <ExpertiseButtons  className="pt-[20px] pb-[235px]"/>
-
+          <Button text="Lets Work Together" width="195px" className='mb-[10px]'/>
+          <ExpertiseButtons  className="pt-[20px]"/>
         </div>
     </div>
   )
