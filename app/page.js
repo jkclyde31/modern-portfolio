b68c517869660177893f1cdb6981f039321524ea
jkClyde <aquin.randall@gmail.com>
2025-01-23 07:04:45 +0800

    debug

--- app/page.js
+++ app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
        <SkillsSection/>
-       <CreativeProjects/>
-       <ContactForm/>
+       {/* <CreativeProjects/>
+       <ContactForm/> */}
 
     </>
    
